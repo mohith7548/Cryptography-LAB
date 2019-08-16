38be21da3bd62cd3d9fd3191f3f5415c6354cfd3
Mohith <mohith7548@gmail.com>
2019-08-16 15:08:38 +0530

    Name corrected

--- Lab-1/Ceaser-Cipher/encrypt.py
+++ Lab-1/Ceaser-Cipher/encrypt.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 
+def Encrypt(K, P):
+    cipher = []
+    for letter in P:
+        cipher.append(chr((ord(letter) - 65 + K) % 26 + 65))
+    return "".join(cipher)
+
+
 def disp(K):
     for i in range(0, 25):
         print(chr(i + 65), end=" ")
@@ -12,19 +19,12 @@ def disp(K):
     print()
 
 
-def Encypt(K, P):
-    cipher = []
-    for letter in P:
-        cipher.append(chr((ord(letter) - 65 + K) % 26 + 65))
-    return "".join(cipher)
-
-
 def main():
     print("Ceaser Cipher Encryption Alg")
     key = int(input("Enter Key: "))
     disp(key)
     plain_text = input("Enter Plain Text: ").upper()
-    cipher_text = Encypt(key, plain_text)
+    cipher_text = Encrypt(key, plain_text)
     print(cipher_text)
 
 
